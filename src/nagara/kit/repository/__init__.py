c1cf75581a21382a15054f9ecb68d256f10a7617
Oluwadamilola Ifedayo <dami.func@gmail.com>
2026-04-25 20:25:21 +0000

    add kit repository base

---
+++ src/nagara/kit/repository/__init__.py
@@ -0,0 +1,11 @@
+from nagara.kit.repository.base import (
+    RepositoryBase,
+    RepositorySoftDeletionMixin,
+    RepositorySortingMixin,
+)
+
+__all__ = [
+    "RepositoryBase",
+    "RepositorySoftDeletionMixin",
+    "RepositorySortingMixin",
+]
